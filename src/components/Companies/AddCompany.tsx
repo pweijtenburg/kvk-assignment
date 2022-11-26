28dc5583f8fe76884275da15bda255107c1bb04c
Patrick Weijtenburg <pweijtenburg@hotmail.com>
2022-11-26 21:39:03 +0100

    tidy up company form

--- src/components/Companies/AddCompany.tsx
+++ src/components/Companies/AddCompany.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, FormEvent, useState} from "react"
-import {Button, TextField} from "@mui/material";
+import {Button, Grid, TextField} from "@mui/material";
 import AddCircle from "@mui/icons-material/AddCircle";
 
 type Props = {
@@ -34,15 +34,30 @@ export default ({saveCompany}: Props) => {
 
     return (
         <form id="company_form" onSubmit={submitHandler} className="new-company-form">
-            <TextField required id="name" label="Company Name" onChange={inputHandler} value={company?.name} />
-            <TextField id="streetName" label="e.g. 5th Avenue 2005" onChange={inputHandler} value={company?.streetName} />
-            <TextField id="city" label="City" onChange={inputHandler} value={company?.city} />
-            <TextField id="zipCode" label="Postal code" onChange={inputHandler} value={company?.zipCode} />
-            <TextField id="logo" label="e.g. https://kompany.nl/our-logo.png" onChange={inputHandler} value={company?.logo} />
-            <Button type="submit" className="search-action" disabled={!company} variant="contained" color="success" disableElevation>
-                <AddCircle />
-                Add Company
-            </Button>
+            <Grid container>
+                <Grid item xs={12}>
+                    <TextField fullWidth required id="name" label="Company Name" onChange={inputHandler} value={company?.name} />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth id="streetName" label="e.g. 5th Avenue 2005" onChange={inputHandler} value={company?.streetName} />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth id="zipCode" label="Postal code" onChange={inputHandler} value={company?.zipCode} />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth id="city" label="City" onChange={inputHandler} value={company?.city} />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth id="logo" label="e.g. https://kompany.nl/our-logo.png" onChange={inputHandler} value={company?.logo} />
+                </Grid>
+                <Grid item xs={12}>
+                    <br />
+                    <Button type="submit" className="search-action" disabled={!company} variant="contained" color="success" disableElevation>
+                        <AddCircle />
+                        Add Company
+                    </Button>
+                </Grid>
+            </Grid>
         </form>
     )
 }
